8291d57eaf86a52214e4948ad2c71f969130f76b
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-22 19:55:58 +0100

    edit do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     time_str = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    path_str = "versions/web_static_{}.tgz".format(time_str)
+    path = "versions/web_static_{}.tgz".format(time_str)
     local("mkdir -p versions")
-    local("tar -cvzf {} web_static".format(path_str))
-    return path_str
+    local("tar -cvzf {} web_static".format(path))
+    return path
