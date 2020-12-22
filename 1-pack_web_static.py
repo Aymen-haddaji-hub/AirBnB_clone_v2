96b9bc4ddb5c2021e35a2fe1c792bf94607422db
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-22 20:08:58 +0100

    delete and re create the function git add 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Task 1 """
+""" abric module to push files"""
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """ Task 1 """
+    """ generates a .tgz archive from the contents of the web_static folder"""
     date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     filepath = "versions/web_static_{}.tgz".format(date)
     local("mkdir -p versions")
