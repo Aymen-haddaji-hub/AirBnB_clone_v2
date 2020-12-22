0b4aaddf425664715216177fdbb158af75a0273a
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-22 19:51:59 +0100

    recorrtct

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!usr/bin/python3
 """ fabric module to push files"""
-import os
 from fabric.api import local
 from datetime import datetime
 
@@ -9,10 +8,6 @@ def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
     time_str = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     path_str = "versions/web_static_{}.tgz".format(time_str)
-    var = "tar -cvzf {} web_static".format(path_str)
     local("mkdir -p versions")
-    local(var)
-    if os.path.exists(path_str):
-        return path_str
-    else:
-        return None
+    local("tar -cvzf {} web_static".format(path_str))
+    return path_str
