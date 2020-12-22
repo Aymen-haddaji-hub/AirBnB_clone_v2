aabf68ab72b251f5b7c5e9184109ad9064866c38
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-12-22 18:48:25 +0100

    deploy web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-# Configuring a new server
-
-sudo apt-get update
+# setting up my web servers for the deployment of web_static
+sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
 sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sfn /data/web_static/current /data/web_static/releases/test/
-sudo chown ubuntu:ubuntu /data
-sudo sed -i '35i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-service nginx restart
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo rm -f -R /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+TEST_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "53i $TEST_STRING" /etc/nginx/sites-available/default
+sudo service nginx restart
