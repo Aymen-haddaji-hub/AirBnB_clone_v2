af9523434d6baf700e09114ec529268b778c0ef7
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-01-18 17:55:39 +0100

    flask web app route dirs return values

---
+++ webflask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" minimal Flask application
+    that return a string """
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    """ main route """
+
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ hbnb directory """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_dir(text):
+    """
+    Return C and the value of var 'text'
+    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python/<text>")
+@app.route("/python/")
+def python_dir(text="is cool"):
+    """
+    Return python and the value of var 'text'
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+if __name__ == '__main__':
+    app.run()
