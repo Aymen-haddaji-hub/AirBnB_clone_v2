ee8a4df4cabd0d2c8bda8ec2aa901ae0b91f1800
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-01-20 19:51:45 +0100

    check_green_please

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+""" minimal Flask application
+    that return a string """
+
+
+from flask import Flask
+from flask import abort
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    """ main route """
+
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ hbnb directory """
+
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_dir(text):
+    """
+    Return C and the value of var 'text'
+    """
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python/<text>")
+@app.route("/python/")
+def python_dir(text="is cool"):
+    """
+    Return python and the value of var 'text'
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route("/number/<n>")
+def number(n):
+    """
+    Returns n, if n is an integer
+    """
+    try:
+        return ("{} is a number".format(int(n)))
+    except Exception:
+        abort(404)
+
+
+@app.route("/number_template/<n>")
+def number_template(n):
+    """
+    display a HTML page only if n is an integer:
+    H1 tag: “Number: n” inside the tag BODY
+    """
+    try:
+        n = int(n)
+        return render_template('5-number.html', n=n)
+    except Exception:
+        abort(404)
+
+
+@app.route("/number_odd_or_even/<n>")
+def number_odd_or_even_template(n):
+    """
+     display a HTML page only if n is an integer:
+    H1 tag: “Number: n is even|odd” inside the tag BODY
+    """
+    try:
+        n = int(n)
+        if n % 2 == 0:
+            s = "even"
+        else:
+            s = "odd"
+        return render_template('6-number_odd_or_even.html', n=n, s=s)
+    except Exception:
+        abort(404)
+
+
+@app.teardown_appcontext
+def tear_down(self):
+    """tear down app context"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """lists states from database
+    Returns:
+        HTML
+    """
+    dict_states = storage.all(State)
+    all_states = []
+    for k, v in dict_states.items():
+        all_states.append(v)
+    return render_template('7-states_list.html', all_states=all_states)
+
+
+if __name__ == '__main__':
+    app.run()
